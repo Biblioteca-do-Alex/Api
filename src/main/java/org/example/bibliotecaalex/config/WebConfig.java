08be1d4a5a9bb4e8c955a67302b4184a5b68de1e
AnaBeatrizRomera <ana.romera@germinare.org.br>
2025-04-01 11:15:28 -0300

    Liberando rotas

--- src/main/java/org/example/bibliotecaalex/config/WebConfig.java
+++ src/main/java/org/example/bibliotecaalex/config/WebConfig.java
@@ -9,10 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/") // Permite CORS para todos os endpoints
-                .allowedOrigins("*") // Permite requisições de qualquer origem
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                .allowedHeaders("*")
-                .allowCredentials(false); // Se true, '*' não pode ser usado em allowedOrigins
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Aqui você coloca o domínio do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*"); // Permite todos os headers
     }
 }
