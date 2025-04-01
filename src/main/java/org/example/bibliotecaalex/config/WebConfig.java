86411718f04eff8bdcd7f592f6b131059c1f23fd
AnaBeatrizRomera <ana.romera@germinare.org.br>
2025-04-01 10:51:08 -0300

    Liberando rotas

---
+++ src/main/java/org/example/bibliotecaalex/config/WebConfig.java
@@ -0,0 +1,22 @@
+package org.example.bibliotecaalex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // permite CORS para todas as rotas
+                        .allowedOrigins("https://arearestritaleontis.s3.us-east-1.amazonaws.com") // Substitua pelo seu domínio
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
